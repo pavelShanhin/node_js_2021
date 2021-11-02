7507b8a04f30dc20cdae9b8ec7486a788555b9e1
pavelShanhin <t606pavel@gmail.com>
2021-11-02 21:24:00 +0300

    feat: add configure foder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi');
+import express from 'express';
+import { PORT,  } from './configure/configure.constants';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+    res.status(400).send({ message: 'This type of request is not handle' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server start on port: ${PORT}`);
+});
