f20120c96354411e8fbb58df8a5ccff0e27ad1f9
pavelShanhin <t606pavel@gmail.com>
2021-12-20 20:43:26 +0300

    feat: add CORS for any domain except locallhost/3000

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,15 +1,19 @@
 import * as express from 'express';
-import { ROUTERS_NAMES } from '../configure';
+import {  ROUTERS_NAMES } from '../configure';
 import { groupRouter, userRouter, groupUserRouter, authRouter } from '../api';
 import { apiErrorHandler } from './error-handler';
 import { logger } from '../loaders/winston';
 import { authService } from '../services/';
+import cors from 'cors';
 
 export const expressLoader = async ({ app }: { app: express.Application }):Promise<void> => {
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
     app.get('/');
+
+    app.use(cors())
+
     app.use(ROUTERS_NAMES.auth, authRouter);
 
     app.use(authService.authentificateUser);
